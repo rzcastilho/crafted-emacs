7ab7a0ac97f6f5caa46341fb260c53625b069954
rzcastilho <rodrigo.zampieri@gmail.com>
2025-04-09 10:53:27 -0300

    refactor: :sparkles: simplify configuration

--- early-init.el
+++ early-init.el
@@ -1,11 +1,2 @@
-;;; early-init.el --- Emacs early initialization for Crafted Emacs (optional) -*- lexical-binding: t; -*-
-;;; Commentary:
-;;
-;;; Code:
-
-;; Configures `package.el'
+;; Set up package archives (configuring `package.el')
 (load "~/crafted-emacs/modules/crafted-early-init-config")
-
-(provide 'early-init)
-
-;;; early-init.el ends here
--- init.el
+++ init.el
@@ -1,60 +1,75 @@
+;; Set up custom.el file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (and custom-file
            (file-exists-p custom-file))
-  (load custom-file nil 'nomessage))
+  (load custom-file nil :nomessage))
 
-;; It crashes the configuration if not initialized here
-(setq package-selected-packages nil)
-
+;; Bootstrap crafted-emacs in init.el
 (load "~/crafted-emacs/modules/crafted-init-config")
 
-;; Select packages to install
-(require 'crafted-completion-packages)
-(require 'crafted-ide-packages)
-(require 'crafted-org-packages)
-(require 'crafted-ui-packages)
+(defun crafted-emacs-load-modules (modules)
+  "Initialize crafted-emacs modules.
 
-;; Custom
-(require 'crafted-programming-packages)
-(require 'crafted-my-org-packages)
-(require 'crafted-theme-packages)
+MODULES is a list of module names without the -packages or
+-config suffixes.  Note that any user-provided packages should be
+added to `package-selected-packages' before invoking this
+function."
+  (dolist (m modules)
+    (require (intern (format "crafted-%s-packages" m)) nil :noerror))
+  (package-install-selected-packages :noconfirm)
+  (dolist (m modules)
+    (require (intern (format "crafted-%s-config" m)) nil :noerror)))
 
-;; Install packages
-(package-install-selected-packages :noconfirm)
+;; Any extra packages in addition to the ones added by crafted-emacs
+;; modules go here before we call `crafted-emacs-load-modules'.
+(customize-set-variable 'package-selected-packages '(
+                                                     ef-themes
+                                                     magit
+                                                     elixir-mode
+                                                     elixir-ts-mode
+                                                     go-mode
+                                                     go-ts-mode
+                                                     mise
+                                                     exec-path-from-shell
+                                                     yasnippet
+                                                     yasnippet-snippets))
 
-;; Load crafted-updates configuration
-(require 'crafted-defaults-config)
-(require 'crafted-osx-config)
-(require 'crafted-startup-config)
-(require 'crafted-ide-config)
-(require 'crafted-org-config)
-(require 'crafted-ui-config)
-(require 'crafted-completion-config)
+(crafted-emacs-load-modules '(defaults completion ui ide osx))
 
-;; Custom
-(require 'crafted-programming-config)
-(require 'crafted-my-org-config)
+(yas-global-mode 1)
+
+;; Dev tools configuration
+(exec-path-from-shell-initialize)
+(add-hook 'after-init-hook #'global-mise-mode)
+(setq mise-update-on-eshell-directory-change t)
+
+;; The first time you run Emacs with this enabled, the
+;; tree sitter parser will be installed for you automatically.
+(crafted-ide-configure-tree-sitter '(elixir go))
 
 ;; Automatically register `eglot-ensure` hooks for relevant major modes
 (crafted-ide-eglot-auto-ensure-all)
 
-;; The first time you run Emacs with this enabled, the
-;; tree sitter parser will be installed for you automatically.
-(crafted-ide-configure-tree-sitter)
+(setq major-mode-remap-alist
+      '((elixir-mode . elixir-ts-mode)
+        (go-mode . go-ts-mode)))
 
-;; Profile emacs startup
-(defun crafted-startup-example/display-startup-time ()
-  "Display the startup time after Emacs is fully initialized."
-  (message "Crafted Emacs loaded in %s."
-           (emacs-init-time)))
-(add-hook 'emacs-startup-hook #'crafted-startup-example/display-startup-time)
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+               '((elixir-mode elixir-ts-mode heex-ts-mode) . ("elixir-ls"))))
 
-(customize-set-variable 'crafted-ui-display-line-numbers t)
+(add-hook 'elixir-ts-mode-hook 'eglot-ensure)
 
-;; Hide unused interface itens
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+;; (add-hook 'prog-mode-hook #'aggressive-indent-mode)
+(add-hook 'prog-mode-hook #'electric-indent-mode)
+(add-hook 'prog-mode-hook #'electric-pair-mode)
+
+;; Close buffer when ends buffer terminal
+(defun term-handle-exit-func (&optional process-name msg)
+  (message "%s | %s" process-name msg)
+  (kill-buffer (current-buffer)))
+
+(advice-add 'term-handle-exit :after 'term-handle-exit-func)
 
 ;; Auto save files
 ;; auto-save-mode doesn't create the path automatically!
@@ -66,18 +81,20 @@
 ;; Disable backup files
 (setq make-backup-files nil)
 
-;; UTF-8 support
-(prefer-coding-system       'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-language-environment   'utf-8)
-(set-keyboard-coding-system 'utf-8-unix)
+;; Hide unused interface itens
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
 
-;; Show column number
-(setq column-number-mode t)
+;; Show line numbers
+(customize-set-variable 'crafted-ui-display-line-numbers t)
 
 ;; Load theme
-(load-theme 'zenburn t)
+(load-theme 'modus-vivendi-deuteranopia)
 
-;; Set font size
-(set-face-attribute 'default nil :height 160)
+;; Font Settings
+(set-face-attribute 'default nil :family "Fira Code" :height 180 :weight 'light)
+
+;; Set transparency
+(set-frame-parameter (selected-frame) 'alpha '(90 90))
+(add-to-list 'default-frame-alist '(alpha 90 90))
